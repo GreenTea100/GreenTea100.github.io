d2603303421c1746affb90907835fbdfd759bc53
Kang Min Seok <to3644101@naver.com>
2022-03-24 04:21:31 +0900

    commit soon

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Title Demo',
+      title: 'My Car',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(title: 'Demo Home Page')
+      home: MyHomePage(title: 'My Car')
     );
   }
 }
@@ -22,10 +22,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
   @override
   Widget build(BuildContext context) {
+    var titleSection = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      Column(
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.all(30)),
+          Text("차량 이름", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),)
+        ],),
+
+    ],);
+
+    var subtitleSection = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(children: <Widget>[
+          Text("누적주행거리", style: TextStyle(fontSize: 16)),
+          Text("0Km", style: TextStyle(fontSize: 16)),
+        ],),
+        Padding(padding: EdgeInsets.all(30)),
+        Column(children: <Widget>[
+          Text("평균 연비", style: TextStyle(fontSize: 16)),
+          Text("0Km", style: TextStyle(fontSize: 16)),
+        ],)
+    ],);
+    var textSection = Text("리스트",);
+
+
     return Scaffold(
-      appBar: AppBar(title:Text(widget.title) ,),
+      appBar: AppBar(title: Text(widget.title),),
+      body: Column(children: <Widget>[
+        //Image.network("https://w.namu.la/s/fe3d77431ce0579466e7b06e5785af8c288f674fa3cc7fd8955ced8af3e63b3ee4782420ac9a3b54b5d2056479055c2663f37b7509b9dbd6122fdde99108d3c07f099598914482d446331ab9380d6070f33d61168ece6128c1e838ea04a46746",height: 240, width: 600,fit:BoxFit.cover),
+        titleSection,
+        subtitleSection,
+        textSection
+      ],),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: const Icon(Icons.drive_eta), label: '홈'),
+          BottomNavigationBarItem(icon: const Icon(Icons.local_gas_station), label: '검색'),
+          BottomNavigationBarItem(icon: const Icon(Icons.account_circle), label: '내정보'),
+
+        ],
+      ),
     );
   }
 }
